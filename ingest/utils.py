6fcc58b75226d60c1a08d9cc06710487dcfc7532
dmitkov28 <mitkov.dimitar@yahoo.com>
2024-09-23 18:58:53 +0300

    add youtube url to video_id util function

--- ingest/utils.py
+++ ingest/utils.py
@@ -1,7 +1,16 @@
 import httpx
+import re
 
 
 def get(url: str):
     res = httpx.get(url)
     if res.status_code == 200:
         return res.json()
+
+
+def get_youtube_video_id_from_url(video_url: str) -> str:
+    pattern = r"v=([^&]+)"
+    match = re.search(pattern, video_url)
+    if match:
+        video_id = match.group(1)
+    return video_id
